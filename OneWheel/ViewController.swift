f9ed65009927f95314b02072508e2100b996babb
David Brodsky <dbro@dbro.pro>
2017-12-30 09:31:27 -0800

    Initial Commit

---
+++ OneWheel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneWheel
+//
+//  Created by David Brodsky on 12/30/17.
+//  Copyright © 2017 David Brodsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
