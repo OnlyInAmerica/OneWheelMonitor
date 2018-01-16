2d830e90304e456a46201f3b724009e5818afa2b
David Brodsky <dbro@dbro.pro>
2018-01-16 07:31:11 -0800

    whitespace

--- OneWheel/AppDelegate.swift
+++ OneWheel/AppDelegate.swift
@@ -42,8 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        NSLog("Flushing bg state queue")
-        owManager.flushBgStateQueue()
+          owManager.flushBgStateQueue()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -53,7 +52,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
